ff3d2a4984f8f362dcec9c33652af84a48a174fb
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-09-09 22:47:49 +0530

    Add C++ program for 2D array input and output

---
+++ CPP/2D_arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int A[3][6];
+
+    cin>> A[3][5]; //A[row][column];
+    cout << A[3][6]; //Output a garbage value 
+    
+
+    return 0;
+}
