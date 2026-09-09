4cc1f5c08a8be7554e44933977ff29059e846b78
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-09-09 20:27:15 +0530

    Add C++ program for 1D array input and output

---
+++ CPP/1D-Arrays.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+    
+    int arr[5];
+
+    cin >> arr[0];
+    cin >> arr[1];
+    cin >> arr[2];
+    cin >> arr[3];
+    cin >> arr[4];
+    cin >> arr[5];
+
+    cout << arr[0]<<endl;    
+    cout << arr[1]<<endl;    
+    cout << arr[2]<<endl;    
+    cout << arr[3]<<endl;    
+    cout << arr[4]<<endl;    
+    cout << arr[5]<<endl;    
+
+    return 0;
+}
