9882a5a70cbfee5e3cc0b27332721c024689858f
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-09-09 20:06:50 +0530

    Add switch case program to display day of the week

---
+++ Pattern/switch_case.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int day;
+    cin >> day;
+
+    switch (day)
+    {
+    case 1:
+        cout << "Monday";
+        break;
+    case 2:
+        break;
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "Wednesday";
+        break;
+
+    case 4:
+        cout << "Thursday";
+        break;
+
+    case 5:
+        cout << "friday";
+        break;
+
+    case 6:
+        cout << "Saturday";
+        break;
+
+    case 7:
+        cout << "Sunday";
+        break;
+        cout <<"Check inside the switch case";
+    default:
+        cout<<"Invalid";
+        
+    }
+    cout <<"Check"<<endl;
+    return 0;
+}
