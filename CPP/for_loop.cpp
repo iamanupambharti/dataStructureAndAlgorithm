03d7f711f679f575ffde8c0ffd5c76ac48910bb8
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-09-09 22:57:29 +0530

    Add C++ program to demonstrate for loop functionality

---
+++ CPP/for_loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; i = i + 1)
+    {
+        cout << "My name is Anupam" << endl<< i << endl;
+    }
+
+    return 0;
+}
