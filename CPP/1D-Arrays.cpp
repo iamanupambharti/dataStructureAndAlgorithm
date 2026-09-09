e28b609224a57bf54dbb9aaf4fdc529f28c0db39
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-09-09 20:30:08 +0530

    Refactor input handling in 1D array program and improve formatting

--- CPP/1D-Arrays.cpp
+++ CPP/1D-Arrays.cpp
@@ -1,24 +1,25 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main (){
-    
+int main()
+{
+
     int arr[5];
 
-    cin >> arr[0];
-    cin >> arr[1];
-    cin >> arr[2];
-    cin >> arr[3];
-    cin >> arr[4];
-    cin >> arr[5];
-
-    cout << arr[0]<<endl;    
-    cout << arr[1]<<endl;    
-    cout << arr[2]<<endl;    
-    cout << arr[3]<<endl;    
-    cout << arr[4]<<endl;    
-    cout << arr[5]<<endl;    
+    // cin >> arr[0];
+    // cin >> arr[1];
+    // cin >> arr[2];
+    // cin >> arr[3];
+    // cin >> arr[4];
+    // cin >> arr[5];
+    cin >> arr[0] >> arr[1] >>arr[2] >>arr[3] >> arr[4] >>arr[5];
+    cout << arr[0] << endl;
+    cout << arr[1] << endl;
+    cout << arr[2] << endl;
+    cout << arr[3] << endl;
+    cout << arr[4] << endl;
+    cout << arr[5] << endl;
 
     return 0;
 }
