bbd3cee9758a60b5a417a7e182a77e91cfd215c5
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-09-10 08:10:44 +0530

    Add C++ program to demonstrate do-while loop functionality

---
+++ CPP/do-while_loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+using namespace std;
+
+int main (){
+    int i = 1;
+    do {
+        cout << "Anupam"<<i<< endl ;
+        i =i+1;
+    }
+    while(i<=5); 
+    cout<< i<<"Aman"<< endl;
+
+return 0;
+}
