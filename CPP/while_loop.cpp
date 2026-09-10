efd5eed5239b9c2a709a68327249d9a9105ad289
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-09-10 08:10:53 +0530

    Add C++ program to demonstrate while loop functionality

---
+++ CPP/while_loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+    int i =1;
+
+    while (i<=5){
+        cout<<"Anupam"<<i<<endl;
+
+
+        i=i+1;
+        }
+
+        return 0;
+}
+
